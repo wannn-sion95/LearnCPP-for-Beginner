5a0b2b6b875ea6d524cca7c9147fa64f68f23f32
Wannn Sion <wannnsion95@gmail.com>
2024-12-24 12:46:07 +0700

    Create same-but-diffrentway.cpp

---
+++ same-but-diffrentway.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int angka;
+    cout << "Masukkan angka pertama: "; cin >> angka;
+    int angka2;
+    cout << "Masukkan angka kedua: "; cin >> angka2;
+    int hasil = angka + angka2;
+    cout << "Hasil penjumlahan dari " << angka << " + " << angka2 <<  " = " <<   hasil  << endl;
+}
